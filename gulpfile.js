9a696512ab84220a99ed22d948963799a474a32c
diego.pazzini2009@hotmail.com <diegopazzini2009@hotmail.com>
2023-07-31 02:11:30 -0300

    criando o hero e importando a font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,24 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default=styles;
+function images() {
+    return gulp.src('./src/images/**/*') //**serve para acessar a pasta scr e imagens  e outro /* serve para pegar os arquivos dentro desta pasta// 
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch= function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
+
+
+
+
+
+
+
+
+
