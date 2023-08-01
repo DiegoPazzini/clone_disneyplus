5baf0d09e79169db9e90a2a387f3260265f1a8e9
diego.pazzini2009@hotmail.com <diegopazzini2009@hotmail.com>
2023-08-01 00:31:35 -0300

    ajuste no mobile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin')
 
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist.js'))
+}
+    
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed'}))
@@ -15,10 +21,11 @@ function images() {
 }
 
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch= function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
 
 
